446226501329a0c3a9a16b82edbaf1a9116ee176
loop-hack <timetraveller1732@gmail.com>
2025-12-11 14:51:32 +0530

    variable

--- MyFirstProgram/src/app/Main.java
+++ MyFirstProgram/src/app/Main.java
@@ -5,9 +5,16 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
+		//This is a comment.
+		
+		/*This is a 
+		  multiple line comments */
+		
 		System.out.print("Fuck off.\n");
 		
 		System.out.print("You piece of shit.");
+		
+		
 
 	}
 
---
+++ MyFirstProgram/src/app/variable.java
@@ -0,0 +1,18 @@
+package app;
+
+public class variable {
+
+	public static void main(String[] args) {
+		
+		
+		//Variable 
+		
+		int age;
+		age = 29;
+		System.out.print(age);
+		
+		
+
+	}
+	
+}
