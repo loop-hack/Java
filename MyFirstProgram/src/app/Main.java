77dfbaec2e1c27bc98da3370e995bee95688de7c
loop-hack <timetraveller1732@gmail.com>
2025-12-11 14:23:23 +0530

    added print line

--- MyFirstProgram/src/app/Main.java
+++ MyFirstProgram/src/app/Main.java
@@ -5,7 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		System.out.print("Fuck off");
+		System.out.print("Fuck off.\n");
+		
+		System.out.print("You piece of shit.");
 
 	}
 
