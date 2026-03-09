0599b91e3e8fcfd1e014fb63e289d7e8b07eb4e5
loop-hack <timetraveller1732@gmail.com>
2026-03-09 21:18:38 +0530

    Default Constructor

---
+++ MyFirstProgram/src/app/ConstructorJava.java
@@ -0,0 +1,18 @@
+package app;
+
+public class ConstructorJava {
+	
+	static class car{
+		String model;
+		int cost;
+	}
+	
+	public static void main(String[] args) {
+		car ferrari = new car();
+		System.out.println(ferrari.model);
+		System.out.println(ferrari.cost);
+		
+	}
+	
+
+}
