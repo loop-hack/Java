cfdd745f88f8ffaff963077ba512ea77f0458b32
loop-hack <timetraveller1732@gmail.com>
2026-04-13 16:06:14 +0530

    learning Singleton class

--- MyFirstProgram/src/oop_lec/oops_lec2.java
+++ MyFirstProgram/src/oop_lec/oops_lec2.java
@@ -2,6 +2,18 @@ package oop_lec;
 
 public class oops_lec2 {
 	
+	
+	static int a = 5;
+	static int b;
+	
+	// This is static block that means it wi9ll run exactly once
+	
+	static {
+		System.out.println("Static Block running. ");
+		b = a * 3;
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		// we only need to make a object when we have class created and method is in it
@@ -21,6 +33,19 @@ public class oops_lec2 {
 		System.out.println(Human_class.population);
 		
 		ank();
+		
+		
+		// we will be using static block here to verify that if its runs single time or it runs multiple time
+		
+		oops_lec2 staticBlock = new oops_lec2();
+		
+		System.out.println(staticBlock.a + " " + staticBlock.b);
+		
+		
+		b += 116;
+		
+		System.out.println(staticBlock.a + " " + staticBlock.b);
+		
 		
 		
 	}
