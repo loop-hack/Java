b6db9116f54494b53af94e4f897f2f1c008b1b9f
loop-hack <timetraveller1732@gmail.com>
2026-04-18 10:51:33 +0530

    using ArrayList

---
+++ MyFirstProgram/src/oop_lec/CollectionMain/Main.java
@@ -0,0 +1,23 @@
+package oop_lec.CollectionMain;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		List <Integer> list1 = new ArrayList<>();
+		List <Integer> list2 = new LinkedList <>();
+		
+		list1.add(2);
+		list2.add(3);
+	
+		System.out.println(list1);
+
+		System.out.println(list2);
+
+		
+	}
+
+}
--- MyFirstProgram/src/oop_lec/exceptionHandling/expDemo.java
+++ MyFirstProgram/src/oop_lec/exceptionHandling/expDemo.java
@@ -37,6 +37,5 @@ public class expDemo {
 		
 		return a/b;
 	}
-	
 
 }
